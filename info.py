a9a2d35808d37cbca47b1f7d46108d33f8cef5f5
photon256 <144157217+photon256@users.noreply.github.com>
2025-02-09 13:57:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['API_ID])
+API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
